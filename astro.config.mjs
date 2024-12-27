5b21f069f6821013e201e58c34de38d256a79b89
joyou73 <joyou94@hotmail.com>
2024-12-27 16:16:20 +0100

    20241227

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://joyhure.github.io/portfolio',
+  base: '/portfolio/',
   devToolbar: {
     enabled: false
   },
